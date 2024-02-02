930d48458b50df9827a616ee83cb559654f50797
John Isik <johnisik314@gmail.com>
2024-02-01 23:34:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ function toggleMenu(){
     menu.classList.toggle("open"); 
     icon.classList.toggle("open");  
 }
+
+function openLinkedInProfile(){
+    window.open('https://www.linkedin.com/in/john-isik/', '_blank');
+}
+function openGithubProfile(){
+    window.open('https://github.com/johnisik314', '_blank');
+}
+function openInstaProfile(){
+    window.open('https://www.instagram.com/johnisik314/', '_blank');
+}
